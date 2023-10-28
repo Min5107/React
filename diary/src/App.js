6ac2596f8f918dc2391b4e33997ac0315763b9c7
JaeMin <90915044+Min5107@users.noreply.github.com>
2023-10-28 19:04:10 +0900

    [1028] 19:00

--- diary/src/App.js
+++ diary/src/App.js
@@ -1,4 +1,6 @@
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Link} from 'react-router-dom';
+import { createContext, useEffect, useReducer, useRef, useState  } from 'react';
+import axios from 'axios';
 import './App.css';
 
 // import RouteTest from './components/RouteTest';
@@ -10,11 +12,116 @@ import Diary from './pages/Diary';
 import MyHeader from './components/MyHeader';
 import MyButton from './components/MyButton';
 
+
+export const DiaryStateContext = createContext(null);
+export const DiaryDispactchContext = createContext(null);
+
+const reducer = (state, action) => {
+  let newState = [];
+  switch(action.type) {
+    case "INIT": {
+      return action.data;
+    }
+    case "CREATE":{
+        const newItem = {
+          ...action.data
+        };
+        newState = [newItem, ...state];
+        break;
+    }
+
+    case "REMOVE": {
+        newState = state.filter((it) => it.id !== action.targetID);
+        break
+    }
+    case "EDIT": {
+        newState = state.map((it) =>
+        it.id === action.data.id ? { ...action.data} : it );
+        break;
+    }
+    default: 
+    return state;
+  }
+  return newState;
+}
+
+
+
+ /* App 데이터  */ 
 function App() {
+  const [data, dispatch] = useReducer(reducer,[]);
+  const dataId = useRef(0);
+  const [dummyData, setDummyData] = useState([]);
+  console.log("App 컴포넌트 렌더링");
+
+//   const initData = res.slice(0, 20).map((res) => {
+//     return {
+//         author: res.email,
+//         content: res.body,
+//         emotion: Math.floor(Math.random() * 5) + 1, // 0~5까지 랜덤수 추출
+//         create_date: new Date().getTime(),
+//         id: dataId.current++ // dataId를 현재 current값으로 넣고나서 ++을 통해 1을 더함
+//     }
+// })
+
+  useEffect(() => {
+    const fetchData = () => {
+      axios.get("https://jsonplaceholder.typicode.com/posts")
+        .then(res => {
+          const initData = res.data.slice(0, 20).map((item) => {
+            return {
+              author: item.email,
+              content: item.body,
+              emotion: Math.floor(Math.random() * 5) + 1, // 0~5까지 랜덤수 추출
+              create_date: new Date().getTime(),
+              id: dataId.current++ // dataId를 현재 current값으로 넣고나서 ++을 통해 1을 더함
+          }
+      })
+          console.log(res.data);
+          setDummyData(initData);
+        });
+    };
+    fetchData();
+  }, []);
+
+
+  const onCreate = (date, content, emotion) => {
+    dispatch({
+      type: "CREATE",
+      data:{
+        id: dataId.current,
+        data: new Date(date).getTime(),
+        content,
+        emotion
+      }
+    });
+    dataId.current += 1;
+  };
+
+  const onRemove = (targetID) => {
+    dispatch({type: "REMOVE", targetID});
+  };
+
+  const onEdit = (targetID, date, content, emotion) => {
+    dispatch({
+      type: "EDIT",
+      data: {
+        id: targetID,
+        data: new Date(date).getTime(),
+        content,
+        emotion
+      }
+    });
+  };
+
   return (
+    <DiaryStateContext.Provider value={{data, dummyData}}>
+      <DiaryDispactchContext.Provider value={(onCreate, onEdit, onRemove)}>
+    
     <BrowserRouter>
     <div className="App">
-      <MyHeader
+            
+      {/* <MyHeader
       leftChild={
         <MyButton
         text={"왼쪽 버튼"}
@@ -56,7 +163,7 @@ function App() {
           text={"버튼"}
           type={"abc"}
           onClick={() => alert("버튼 클릭")}
-        />
+        /> */}
 
       <Routes>
         <Route path='/' element={<Home/>}/>
@@ -70,7 +177,10 @@ function App() {
       </Link>
        */}
     </div>
+    
     </BrowserRouter>
+    </DiaryDispactchContext.Provider>
+    </DiaryStateContext.Provider>
   );
 }
 
--- diary/src/pages/Home.js
+++ diary/src/pages/Home.js
@@ -1,8 +1,65 @@
+import { useState, useContext } from "react";
+import MyHeader from "../components/MyHeader";
+import MyButton from "../components/MyButton";
+import { DiaryStateContext } from "../App";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+
+
 const Home = () => {
-    return ( <div> 
-        <h1>Home</h1> 
-        <p>이곳은 Home 입니다</p>
+    const [curDate, setCurDate] = useState(new Date());
+    console.log(curDate);
+
+    const curText = `${curDate.getFullYear()} 년 ${curDate.getMonth() + 1} 월` 
+
+    const handlePrevMonth = () => {
+        setCurDate(
+            new Date(curDate.getFullYear(), curDate.getMonth() - 1, curDate.getDate())
+        )
+    };
+
+    const handleNextMonth = () => {
+        setCurDate(
+            new Date(curDate.getFullYear(), curDate.getMonth() + 1, curDate.getDate())
+        )
+    };
+
+    const { dummyData } = useContext(DiaryStateContext);
+    const [startDate, setStartDate] = useState(new Date());
+    
+  
+    
+    return (<div> 
+
+         <div>
+            <MyHeader leftChild={<MyButton text={"<"} onClick={handlePrevMonth}/>} 
+        headText={curText} 
+        rightChild={<MyButton text={">"} onClick={handleNextMonth}/>}/>
+            </div>
+            <DatePicker
+  
+  selected={startDate} // 처음에 맨 위에 표시된 input에 나오는게 지금 날짜
+  onChange={(date) => setStartDate(date)} // 내가 선택한 날짜가 맨 위에 표시 됨
+  showTimeSelectOnly
+  showTimeSelect // 시간 나오게 하기
+  timeFormat="HH:mm" //시간 포맷 
+  timeIntervals={30} // 15분 단위로 선택 가능한 box가 나옴
+  timeCaption="time"
+  dateFormat="HH:mm"
+
+ />
+            {dummyData.map(it => (
+  <div key={it.id}>
+    <p>{it.content}</p>
+    <p>{it.author}</p>
+  </div>
+))}
+            
+           
         </div>
+
+       
     );
 };
 
