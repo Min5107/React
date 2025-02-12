9bf5293247914ea7ad98c23955e8627e214aaef4
JaeMin <90915044+Min5107@users.noreply.github.com>
2025-02-12 23:17:48 +0900

    [23:15] useStateUpdate

--- learn-react/src/components/Counter.jsx
+++ learn-react/src/components/Counter.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
 
+// 일반적 사용
 function Counter({onTotal}){
   const [counter, setCounter] = useState(0);
 
   const handleCounter = () =>{
-    setCounter(counter + 1);
+    setCounter((prevCounter) => prevCounter + 1);
+    setCounter((prevCounter) => prevCounter + 1);
+    setCounter((prevCounter) => prevCounter + 1);
     if(onTotal){
       onTotal();
     }
