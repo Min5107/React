73d843f3d0f406d7215bc7c774d1c696dd1d58b9
JaeMin <90915044+Min5107@users.noreply.github.com>
2025-02-27 22:56:21 +0900

    [22:55] Memo

--- learn-react/src/main.jsx
+++ learn-react/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './AppRef.jsx'
+import App from './AppMemo.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
